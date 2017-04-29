4b62c401be208bdab51a61d6c0ac9208ecdf14fd
wangqingming <wangqingming@bbktel.com>
2017-04-29 16:47:32 +0800

    modify bootstrap base on tomahawk

--- themes/bootstrap/webapp/bootstrap/js/dropdown.js
+++ themes/bootstrap/webapp/bootstrap/js/dropdown.js
@@ -1,4 +1,4 @@
-<#--
+/*
 Licensed to the Apache Software Foundation (ASF) under one
 or more contributor license agreements.  See the NOTICE file
 distributed with this work for additional information
@@ -15,4 +15,27 @@ software distributed under the License is distributed on an
 KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
--->
+*/
+
+
+var DropDownMenu = (
+    function(menuElement) {
+    var menuTitle = menuElement.find("h2:first");
+    menuElement.children().each(function(node){
+      // if there is a submenu
+      var submenu = jQuery(this).find("ul:first");
+
+      if(submenu != null){
+        // make sub-menu invisible
+        submenu.hide();
+        // toggle the visibility of the submenu
+        if (menuTitle != null) {
+          menuTitle.mouseover (function(){ submenu.css({'display': 'block'});});
+          menuTitle.mouseout (function(){submenu.hide();});
+        }
+        jQuery(this).mouseover (function(){submenu.css({'display': 'block'});});
+        jQuery(this).mouseout (function(){submenu.hide();});
+      }
+    });
+  }
+);
